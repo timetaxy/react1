3c55683ecfa3f7032865d7ded46c4602cdfdf682
lsh <>
2021-05-26 10:26:38 +0900

    rebuild

--- app1/src/Movies.js
+++ app1/src/Movies.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import _ from "./Movie.css";
+// import _ from "./Movie.css";
 
 // function Movie({ id, year, title, summary, poster }) {
 function Movie({ year, title, summary, poster, genres }) {
